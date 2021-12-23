ca777ba533ccf376683a042ae546c030a88a9448
Abdul Halim <ahr.web.pro@gmail.com>
2021-12-23 20:04:34 +0600

    React app for loading twitter quotes randomly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './App.scss';
 
 function App() {
+
+  const colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', '#472E32', '#BDBB99', '#77B1A9', '#73A857'];
+
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState({});
+  const [color, setColor] = useState(colors[0]);
+
+  useEffect(() => {
+    async function getQuotes() {
+      const data = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
+      const qt = await data.json();
+      setQuotes([...qt.quotes]);
+      setQuote(qt.quotes[0]);
+    }
+    getQuotes();
+  }, []);
+
+  const changeQuote = () => {
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const colorIndex = Math.floor(Math.random() * colors.length);
+    setQuote(quotes[quoteIndex]);
+    setColor(colors[colorIndex]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper" style={{ backgroundColor: color }}>
+      <div id="quote-box" className="card text-center">
+        <h1 id="text" className="mb-0" style={{ color: color }}>
+          <i className="fas fa-quote-left me-2"></i>{quote.quote}
+        </h1>
+        <p id="author" className="text-end mb-0 mt-4" style={{ color: color }}>- {quote.author}</p>
+        <div className="d-flex justify-content-between align-items-center mt-4">
+          <a href="https://twitter.com/intent/tweet" target="_top" id="tweet-quote"><i className="fab fa-twitter" style={{ backgroundColor: color }}></i></a>
+          <button type="button" id="new-quote" className="btn btn-quote" onClick={changeQuote} style={{ backgroundColor: color }}>New Quote</button>
+        </div>
+      </div>
     </div>
   );
+
 }
 
 export default App;
